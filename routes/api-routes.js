776047ed3ae41dab0c4f0cd26b250a11c84b6db9
Theresa E <t.eatherly@gmail.com>
2020-05-12 12:23:22 -0500

    added routes within api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,19 +1,44 @@
 const db = require('../models');
 
 module.exports = function (app) {
-
-    // POST route for saving a new todo. You can create a todo using the data on req.body
-    app.post("/api/SOMETHING", function (req, res) {
-
+    // POST route for saving a new game. You can create a game using the data on req.body
+    app.post('/api/SOMETHING', function (req, res) {
+        db.Games.create(req.body)
+        .then(function(dbGame){
+            res.json(dbGame);
+          })
+            .catch(function(err) {
+                res.json(err);
+            })
     });
 
-    // DELETE route for deleting todos. You can access the todo's id in req.params.id
-    app.delete("/api/SOMETHING/:id", function (req, res) {
-
+    // DELETE route for deleting game. You can access the game's id in req.params.id
+    app.delete('/api/SOMETHING/:id', function (req, res) {
+        db.Games.destroy( {
+            where: {
+               id: req.params.id 
+            }
+        })
+        .then(function(){
+            res.json(true);
+          })
+            .catch(function(err) {
+                res.json(err);
+            })
     });
 
-    // PUT route for updating todos. The updated todo will be available in req.body
-    app.put("/api/SOMETHING", function (req, res) {
-
-    });
-}
+    // PUT route for updating game. The updated game will be available in req.body
+    app.put('/api/SOMETHING/:id', function (req, res) {
+        db.Games.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(function(dbGame){
+            res.json(dbGame);
+          })
+            .catch(function(err) {
+                res.json(err);
+            })
+        });
+};
