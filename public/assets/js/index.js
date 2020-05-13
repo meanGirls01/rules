2bb884046c66832abf13b9820c7d0ffe96a4111c
Josh Everett <josheverett93@gmail.com>
2020-05-13 18:57:45 -0500

    get call working for games in public index.js file

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -7,19 +7,19 @@ $(document).ready(function () {
     event.preventDefault();
     let searchTerm = input.val().trim();
 
-        $.get('/api/' + searchTerm, function (data) {
-            if (data.length !== 0) {
+    $.get('/api/' + searchTerm, function (data) {
+      if (data.length !== 0) {
 
-                console.log(data)
-                for (var i = 0; i < data.length; i++) {
-                    $('.game-title').text(data[i].title);
-                    $('.category').text(data[i].category)
-                    $('.country').text(data[i].country_origin)
-                    $('.gamedescription').text(data[i].description)
-                    $('.gameinstructions').text(data[i].instructions)
-                };
-            }
-        });
-    })
+        console.log(data);
+        for (var i = 0; i < data.length; i++) {
+          $('.game-title').text(data[i].title);
+          $('.category').text(data[i].category);
+          $('.country').text(data[i].country_origin);
+          $('.gamedescription').text(data[i].description);
+          $('.gameinstructions').text(data[i].instructions);
+        };
+      }
+    });
+  });
 
 });
