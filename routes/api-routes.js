5e9f5c6b5de1a3c73454764dbee330c6f0f26db0
sammiefrog <sammiefrog@live.com>
2020-05-13 18:13:58 -0500

    travis finishing

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,86 +2,86 @@ const db = require('../models');
 
 module.exports = function (app) {
 //find all games
-      app.get('/api/all', function (req, res) {
-          db.Games.findAll({}).then(function (results) {
-              res.json(results);
-          });
-      });
+  app.get('/api/all', function (req, res) {
+    db.Games.findAll({}).then(function (results) {
+      res.json(results);
+    });
+  });
 
-    app.get('/api/:title', function (req, res) {
-        console.log(req.params);
-        db.Games.findAll({
-            where: {
-                title: req.params.title
-            }
-        }).then(function (dbGame) {
-            res.json(dbGame);
-        }).catch(function (err) {
-            res.json(err);
-        });
+  app.get('/api/:title', function (req, res) {
+    console.log(req.params);
+    db.Games.findAll({
+      where: {
+        title: req.params.title
+      }
+    }).then(function (dbGame) {
+      res.json(dbGame);
+    }).catch(function (err) {
+      res.json(err);
+    });
+  });
+
+  app.get('/api/games/:id', function (req, res) {
+    db.Games.findOne({
+      where: {
+        id: req.params.id
+      }
     })
+      .then(function (dbGame) {
+        res.json(dbGame);
+      })
+      .catch(function (err) {
+        res.json(err);
+      });
+  });
 
-    app.get('/api/games/:id', function (req, res) {
-        db.Games.findOne({
-            where: {
-                id: req.params.id
-            }
-            })
-            .then(function (dbGame) {
-                    res.json(dbGame);
-                })
-            .catch(function (err) {
-                res.json(err);
-                });
-    });
+  // POST route for saving a new game. You can create a game using the data on req.body
+  app.post('/api/newgame', function (req, res) {
+    console.log(req.body);
+    console.log(req.params);
 
-    // POST route for saving a new game. You can create a game using the data on req.body
-    app.post('/api/newgame', function (req, res) {
-        console.log(req.body);
-        console.log(req.params);
+    db.Games.create({
+      title: req.body.title, 
+      description: req.body.description,
+      country_origin: req.body.country_origin,
+      category: req.body.category,
+      instructions: req.body.instructions
+    })
+      .then(function(dbGame){
+        res.json(dbGame);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
 
-        db.Games.create({
-            title: req.body.title, 
-            description: req.body.description,
-            country_origin: req.body.country_origin,
-            category: req.body.category,
-            instructions: req.body.instructions
-        })
-        .then(function(dbGame){
-            res.json(dbGame);
-          })
-            .catch(function(err) {
-                res.json(err);
-            })
-    });
+  // DELETE route for deleting game. You can access the game's id in req.params.id
+  app.delete('/api/games/:id', function (req, res) {
+    db.Games.destroy( {
+      where: {
+        id: req.params.id 
+      }
+    })
+      .then(function(){
+        res.json(true);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
 
-    // DELETE route for deleting game. You can access the game's id in req.params.id
-    app.delete('/api/games/:id', function (req, res) {
-        db.Games.destroy( {
-            where: {
-               id: req.params.id 
-            }
-        })
-        .then(function(){
-            res.json(true);
-          })
-            .catch(function(err) {
-                res.json(err);
-            })
-    });
-
-    // PUT route for updating game. The updated game will be available in req.body
-    app.put('/api/games/:id', function (req, res) {
-        db.Games.update(req.body, {
-            where: {
-                id: req.params.id
-            }
-        })
-        .then(function(dbGame){
-            res.json(dbGame);
-          })
-            .catch(function(err) {
-                res.json(err);
-            })
-        });
+  // PUT route for updating game. The updated game will be available in req.body
+  app.put('/api/games/:id', function (req, res) {
+    db.Games.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function(dbGame){
+        res.json(dbGame);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
 };
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,16 +2,16 @@ const path = require('path');
 
 module.exports = function (app) {
 
-    app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, 'index.html'));
-    });
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
 
-    app.get('/about', function (req, res) {
-        res.sendFile(path.join(__dirname, 'about.html'));
-    });
+  app.get('/about', function (req, res) {
+    res.sendFile(path.join(__dirname, 'about.html'));
+  });
 
-    app.get('/addgame', function (req, res) {
-        res.sendFile(path.join(__dirname, 'addGame.html'));
-    });
+  app.get('/addgame', function (req, res) {
+    res.sendFile(path.join(__dirname, 'addGame.html'));
+  });
 
 };
