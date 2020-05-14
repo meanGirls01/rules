5c57d93977543f40612212d8be5e178ef8a6e39d
charlenne alexander <charlennep@gmail.com>
2020-05-14 10:08:19 -0500

    mobile friendly complete. needed to add a js code to make the nav bar work. also put the line of code for the colpase where has to be on the js. has to be the last thing document.ready do.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,25 +1,31 @@
 $(document).ready(function () {
-  $('.collapsible').collapsible();
-  let search = $('#search');
-  let input = $('.input');
-
-  search.on('click', function (event) {
-    event.preventDefault();
-    let searchTerm = input.val().trim();
-
-    $.get('/api/' + searchTerm, function (data) {
-      if (data.length !== 0) {
-
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-          $('.game-title').text(data[i].title);
-          $('.category').text(data[i].category);
-          $('.country').text(data[i].country_origin);
-          $('.gamedescription').text(data[i].description);
-          $('.gameinstructions').text(data[i].instructions);
-        };
-
-      }
+    let burger = document.querySelector('.navbar-burger');
+    let menu = document.querySelector('.navbar-menu');
+    burger.addEventListener('click', function () {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
     });
-  });
+
+    let search = $('#search');
+    let input = $('.input');
+
+    search.on('click', function (event) {
+        event.preventDefault();
+        let searchTerm = input.val().trim();
+
+        $.get('/api/' + searchTerm, function (data) {
+            if (data.length !== 0) {
+                console.log(data);
+                for (var i = 0; i < data.length; i++) {
+                    $('.game-title').text(data[i].title);
+                    $('.category').text(data[i].category);
+                    $('.country').text(data[i].country_origin);
+                    $('.gamedescription').text(data[i].description);
+                    $('.gameinstructions').text(data[i].instructions);
+                }
+            }
+        });
+    });
+    // making the cards on index collapsable, has to be here because we are creating the card with string literals
+    $('.collapsible').collapsible();
 });
